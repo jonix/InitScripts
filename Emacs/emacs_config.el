5afacd1ced37af1db501d5012834a30fb3bf560c
Jonatan Andersson <jonix@komputix.(none)>
2010-08-09 14:28:54 +0200

    [Emacs Bundle] - Added support to auto bytecompile installed plugins.

--- Emacs/emacs_config.el
+++ Emacs/emacs_config.el
@@ -14,6 +14,13 @@
 
 (add-to-list 'load-path "~/.InitScripts/Emacs/Plugins")
 
+;; Auto byte-compile lisp plugins
+(require 'auto-async-byte-compile)
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+
+
+
 ;; The startup screen is annoying.
 (setq inhibit-startup-message t)
 
