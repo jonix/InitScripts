69553faeed0324b5c766b52fd36b6bcaa73f077f
Jonatan Andersson <jonix@komputix.(none)>
2010-08-09 11:38:01 +0200

    [Emacs Bundle] - Switched default abbrevation mode to (M-\) to hippie-expand

--- Emacs/emacs_config_keybindings.el
+++ Emacs/emacs_config_keybindings.el
@@ -19,6 +19,10 @@
 (global-set-key (kbd "C-;") 'comment-region); Comment a selection
 (global-set-key (kbd "C-:") 'uncomment-region); Remove comments from a section
 
+;; Alternative abbrevation expand
+;; I don't see any advantages from simple abbrev-expand
+(global-set-key (kbd "M-/") 'hippie-expand)
+
 
 (global-set-key "\M-\C-r" `query-replace-regexp) 		; regexp replace
 
