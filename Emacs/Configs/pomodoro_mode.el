3190cfaf45b2e81508066e972a372e89fc776bd1
Jonatan Andersson <jonatan.andersson@cycoresystems.com>
2010-09-10 11:32:59 +0200

    [Emacs Bundle] - Day planning experiments with org-mode + etc modes.

---
+++ Emacs/Configs/pomodoro_mode.el
@@ -0,0 +1,12 @@
+;; This config is written in emacs_config directly
+
+;; http://orgmode.org/worg/org-gtd-etc.php
+;; http://www.bugfree.dk/blog/2009/11/05/getting-organized-with-emacs-org-mode/
+;; http://www.43folders.com/2004/09/08/getting-started-with-getting-things-done
+
+
+;; (add-to-list 'org-modules 'org-timer)
+;; (setq org-timer-default-timer 25)
+;; (add-hook 'org-clock-in-hook '(lambda ()
+;; 								(if (not org-timer-current-timer)
+;; 									(org-timer-set-timer '(16)))))
