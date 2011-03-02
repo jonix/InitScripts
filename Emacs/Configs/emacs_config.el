034ba6990e9b8b64cc1ce9c2086b4239deb0919d
Jonatan Andersson <jonix@komputix.(none)>
2011-03-02 16:53:53 +0100

    [Emacs Bundle] - Added function to revert all open buffers with one command

--- Emacs/Configs/emacs_config.el
+++ Emacs/Configs/emacs_config.el
@@ -639,3 +639,17 @@ buffer read-only, so I suggest setting kill-read-only-ok to t."
 		 (expand-file-name
 			(ido-completing-read
 			 "Project file: " (tags-table-files) nil t)))))
+
+(defun revert-all-buffers ()
+	"Refreshes all open buffers from their respective files"
+	(interactive)
+	(let* ((list (buffer-list))
+				 (buffer (car list)))
+		(while buffer
+			(when (and (buffer-file-name buffer)
+								 (not (buffer-modified-p buffer)))
+				(set-buffer buffer)
+				(revert-buffer t t t))
+			(setq list (cdr list))
+			(setq buffer (car list))))
+	(message "Refreshed open files"))
