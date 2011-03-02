b9c1ed9b6d3c433983e7ceefdb5685bc0a2eff83
Jonatan Andersson <jonix@komputix.(none)>
2011-03-02 14:38:37 +0100

    [Emacs] - Re-enabled C++ compile shortcut (C-cc)

--- Emacs/Configs/emacs_config_keybindings.el
+++ Emacs/Configs/emacs_config_keybindings.el
@@ -3,10 +3,10 @@
 ;; Keystroke customization (functions)
 
 ;; For compilations
-;; (global-set-key "\C-cc" 'compile)			; to compile from within Emacs
-(global-set-key "\C-cv" 'recompile)						; to compile from within Emacs
-(global-set-key "\C-c." 'next-error)   					; Go to next error
-(global-set-key "\C-c," 'previous-error)				; Go to previous error
+(global-set-key "\C-cc" 'compile)         ; to compile from within Emacs
+(global-set-key "\C-cv" 'recompile)       ; to compile from within Emacs
+(global-set-key "\C-c." 'next-error)   		; Go to next error
+(global-set-key "\C-c," 'previous-error)	; Go to previous error
 
 ;; For line jump to
 (global-set-key "\C-l" 'goto-line) ; Go to a specified line number
