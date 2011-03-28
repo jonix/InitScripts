45ed93e287db6c036c8d3b8812b65756fa0f6dbb
Jonatan Andersson <jonix@komputix.(none)>
2011-03-28 16:35:16 +0200

    [Bash] - Removed swedish GDM customizations.

--- Bash/linux_settings.sh
+++ Bash/linux_settings.sh
@@ -5,8 +5,8 @@
 echo "Loading Linux specific settings"
 
 # Set locale for Swedish date and time format
-LANG=sv_SE.utf8
-GDM_LANG=sv_SE.utf8
+#LANG=sv_SE.utf8
+#GDM_LANG=sv_SE.utf8
 
 export PATH=/usr/lib/ccache/:$PATH
 
