6a01bde02f77e496e5603d6db4a2d10230169e53
Jonatan Andersson <jonix@cycoresystems.com>
2011-04-07 14:16:40 +0200

    [Emacs] - Tried and (failed) to remove the annoying backgrounds color in PHP functions.

--- Emacs/Configs/php_mode.el
+++ Emacs/Configs/php_mode.el
@@ -1,3 +1,9 @@
 (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+
+;; Remove inherited background
+;;  http://stackoverflow.com/questions/3042123/in-emacs-how-to-stop-nxthml-to-mess-with-my-background-color
+(setq mumamo-background-colors nil)
+;; (set-face-background "mmm-default-submode-face" nil)
+;; (set-face-background 'mmm-default-submode-face "gray16")
