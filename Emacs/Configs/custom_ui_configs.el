53a63a44127611dcb3603948f26bb7698a9a2296
Jonatan Andersson <jonix@Jonatans-MacBook-Pro.local>
2012-09-22 15:10:49 +0200

    ALL YOUR BASES ARE BELONG TO US!!!! All these files has changed permission to 0755

--- Emacs/Configs/custom_ui_configs.el
+++ Emacs/Configs/custom_ui_configs.el
@@ -4,10 +4,11 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
- '(aquamacs-customization-version-id 206 t)
+ '(aquamacs-customization-version-id 215 t)
  '(aquamacs-tool-bar-user-customization nil t)
  '(completion-ignored-extensions (quote (".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "Build/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".fasl" ".ufsl" ".fsl" ".dxl" ".pfsl" ".dfsl" ".p64fsl" ".d64fsl" ".dx64fsl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo")))
  '(cua-mode t nil (cua-base))
+ '(default-frame-alist (quote ((menu-bar-lines . 1) (foreground-color . "Black") (background-color . "White") (cursor-type . box) (cursor-color . "Red") (vertical-scroll-bars . right) (internal-border-width . 0) (left-fringe . 1) (right-fringe) (fringe) (tool-bar-lines . 0))))
  '(display-time-mode t)
  '(ecb-compile-window-height 6)
  '(ecb-options-version "2.40")
@@ -23,11 +24,26 @@
  '(save-place t nil (saveplace))
  '(semantic-idle-scheduler-idle-time 3)
  '(semantic-self-insert-show-completion-function (lambda nil (semantic-ia-complete-symbol-menu (point))))
- '(show-paren-mode t)
- '(vc-handled-backends (quote (Git SVN Bzr CVS Hg Mtn Arch))))
+ '(vc-handled-backends (quote (Git SVN Bzr CVS Hg Mtn Arch)))
+ '(visual-line-mode nil t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(flymake-errline ((t (:background "black" :foreground "yellow")))))
+ '(flymake-errline ((t (:background "black" :foreground "yellow"))))
+ '(latex-mode-default ((t (:inherit text-mode-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 130 :width normal :family "Monaco")))))
+
+;; Check custom-file compatibility
+(when (and (boundp 'aquamacs-version-id)
+           (< (floor (/ aquamacs-version-id 10))
+	   (floor (/ aquamacs-customization-version-id 10))))
+  (defadvice frame-notice-user-settings (before show-version-warning activate)
+    (defvar aquamacs-backup-custom-file nil "Backup of `custom-file', if any.")
+    (setq aquamacs-backup-custom-file "~/.InitScripts/Emacs/Configs/customizations.2.0.el")
+    (let ((msg "Aquamacs options were saved by a more recent program version.
+Errors may occur.  Save Options to overwrite the customization file. The original, older customization file was backed up to ~/.InitScripts/Emacs/Configs/customizations.2.0.el."))
+      (if window-system
+	  (x-popup-dialog t (list msg '("OK" . nil) 'no-cancel) "Warning")
+	(message msg)))))
+;; End compatibility check
