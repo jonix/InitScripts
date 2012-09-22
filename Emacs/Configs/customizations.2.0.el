560aa766b09d72c6a680dd7f3bc45edd403ace3e
Jonatan Andersson <jonix@Jonatans-MacBook-Pro.local>
2012-09-22 15:08:58 +0200

    [Emacs] - Aquamacs Latex font changed to Monaco 12 pt

---
+++ Emacs/Configs/customizations.2.0.el
@@ -0,0 +1,33 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 206 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(completion-ignored-extensions (quote (".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "Build/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".fasl" ".ufsl" ".fsl" ".dxl" ".pfsl" ".dfsl" ".p64fsl" ".d64fsl" ".dx64fsl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo")))
+ '(cua-mode t nil (cua-base))
+ '(display-time-mode t)
+ '(ecb-compile-window-height 6)
+ '(ecb-options-version "2.40")
+ '(ecb-source-path (quote ("~/Projects/Work/Mapiton/Native/Source" ("/home/jonix/Projects/Work/Mapiton/Native/Source/WatchBoar/Source" "WatchBoar"))))
+ '(isearch-allow-scroll t)
+ '(js2-auto-indent-p t)
+ '(js2-enter-indents-newline t)
+ '(ns-alternate-modifier (quote super))
+ '(ns-command-modifier (quote meta))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(php-manual-path "~/Documents/PHPDoc/")
+ '(save-place t nil (saveplace))
+ '(semantic-idle-scheduler-idle-time 3)
+ '(semantic-self-insert-show-completion-function (lambda nil (semantic-ia-complete-symbol-menu (point))))
+ '(show-paren-mode t)
+ '(vc-handled-backends (quote (Git SVN Bzr CVS Hg Mtn Arch))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(flymake-errline ((t (:background "black" :foreground "yellow")))))
