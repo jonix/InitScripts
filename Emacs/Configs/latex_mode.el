b203c4499497c375ff5dd8d7813f8feaef2f320d
Jonatan Andersson <jonix@Jonatans-MacBook-Pro.local>
2012-10-10 23:43:19 +0200

    [Emacs] - Added Latex support + Realtime Latex compilation&View
    Split Emacs into two buffers
    In first frame open the Latex file
    In second frame open the PDF file
    
    Compile the latex code, and the PDF file inside
    should automatically update
    
    Otherwise use latexmk -pvc … command

--- Emacs/Configs/latex_mode.el
+++ Emacs/Configs/latex_mode.el
@@ -17,15 +17,26 @@
 
 ;; use Skim as default pdf viewer
 ;; Skim's displayline is used for forward search (from .tex to .pdf)
-;; option -b highlights the current line; option -g opens Skim in the background  
-(setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
-(setq TeX-view-program-list
-     '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+;; option -b highlights the current line; option -g opens Skim in the background
+;; (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+;; (setq TeX-view-program-list
+;;     '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
 
-(server-start); start emacs in server mode so that skim can talk to it
+;; (server-start); start emacs in server mode so that skim can talk to it
 
 ;; Put this in ~/.latexmkrc
 ;;
 ;; $pdflatex = 'pdflatex -interaction=nonstopmode -synctex=1 %O %S';
 ;; $pdf_previewer = 'open -a skim';
 ;; $clean_ext = 'bbl rel %R-blx.bib %R.synctex.gz';
+
+;; For previewing math formulas inside Emacs
+;;(autoload 'latex-math-preview-expression "latex-math-preview" nil t)
+;;(autoload 'latex-math-preview-insert-symbol "latex-math-preview" nil t)
+;;(autoload 'latex-math-preview-save-image-file "latex-math-preview" nil t)
+;;(autoload 'latex-math-preview-beamer-frame "latex-math-preview" nil t)
+
+;; For Doc-view to automatically reload a changed PDF file in Emacs
+(add-to-list 'auto-mode-alist '("\\.pdf\\'" . doc-view-mode))
+(add-hook 'doc-view-mode-hook 'auto-revert-mode)
+
