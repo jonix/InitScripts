98e1e41d20b37ddd658d21cd2ced5487e79c4c0f
Jonatan Andersson <jonix@Jonatans-MacBook-Pro.local>
2013-01-26 18:00:27 +0100

    [Bash] - Terminal shortcuts for studies cd:ing

--- Bash/studies_shortcuts.sh
+++ Bash/studies_shortcuts.sh
@@ -1,5 +1,6 @@
 ### Shortcuts for navigating to Studies folder
-alias fyskand='cd "${HOME}/Documents/FysKand/HT 2012"'
-alias mekanik='cd "${HOME}/Documents/FysKand/HT 2012/Mekanik"'
-alias geometri='cd ${HOME}/Documents/FysKand/HT\ 2012/Geometri\ \&\ Analys/'
-alias intro='cd "${HOME}/Documents/FysKand/HT 2012/Mekanik"'
+alias fyskand='cd "${HOME}/Documents/FysKand/HT-2012"'
+alias mekanik='cd "${HOME}/Documents/FysKand/HT-2012/Mekanik"'
+alias geometri='cd ${HOME}/Documents/FysKand/HT-2012/Geometri\ \&\ Analys/'
+alias intro='cd "${HOME}/Documents/FysKand/HT-2012/Introduktion till fysiken"'
+alias bervet='cd "${HOME}/Documents/FysKand/HT-2012/BerakningsVetenskap"'
