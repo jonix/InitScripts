9bbe476d92b1f5abe80ab1a28c00808eff3811ef
Jonatan Andersson <jonix@JonixMacBookPro.local>
2014-04-06 12:42:23 +0200

    [Emacs] - Updated Yasnippet to a newer version

--- Emacs/Configs/yasnippets_config.el
+++ Emacs/Configs/yasnippets_config.el
@@ -1,4 +1,4 @@
 ;; For YASnippet (TextMate like snippets, think "cl+<Tab>" for a full C++ class definition)
 (require 'yasnippet) ;; not yasnippet-bundle
-(yas/initialize)
-(yas/load-directory "~/.InitScripts/Emacs/Plugins/yasnippet/snippets")
+(yas-global-mode 1)
+(yas/load-directory "~/.InitScripts/Emacs/Plugins/snippets")
