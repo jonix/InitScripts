cfc140368a96e4a7c7e2fda21d318df01cc7315a
Jonatan Andersson <jonix@JonixMacBookPro.local>
2014-04-17 00:37:51 +0200

    [Emacs] - Added initial Python config

---
+++ Emacs/Configs/python_config.el
@@ -0,0 +1,7 @@
+;(add-to-list 'load-path "~/.InitScripts/Emacs/Configs/python") ;; tell where to load the various files
+;(require 'epy-setup)      ;; It will setup other loads, it is required!
+;(require 'epy-python)     ;; If you want the python facilities [optional]
+;(require 'epy-completion) ;; If you want the autocompletion settings [optional]
+;(require 'epy-editing)    ;; For configurations related to editing [optional]
+;(require 'epy-bindings)   ;; For my suggested keybindings [optional]
+;(require 'epy-nose)       ;; For nose integration
