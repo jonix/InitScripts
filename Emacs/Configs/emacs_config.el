d089e01ac0974e3ab51269e7c61b6950ed0298d7
Jonatan Andersson <jonix@JonixMacBookPro.local>
2014-04-18 15:06:07 +0200

    [Emacs] - Search and copy words at cursor position

--- Emacs/Configs/emacs_config.el
+++ Emacs/Configs/emacs_config.el
@@ -222,6 +222,36 @@
 											(paren-toggle-open-paren-context 1))))
 
 
+;; Search word at point
+;;;
+;; Move to beginning of word before yanking word in isearch-mode.
+;; Make C-s C-w and C-r C-w act like Vim's g* and g#, keeping Emacs'
+;; C-s C-w [C-w] [C-w]... behaviour.
+(require 'thingatpt)
+
+(defun my-isearch-yank-word-or-char-from-beginning ()
+  "Move to beginning of word before yanking word in isearch-mode."
+  (interactive)
+  ;; Making this work after a search string is entered by user
+  ;; is too hard to do, so work only when search string is empty.
+  (if (= 0 (length isearch-string))
+      (beginning-of-thing 'word))
+  (isearch-yank-word-or-char)
+  ;; Revert to 'isearch-yank-word-or-char for subsequent calls
+  (substitute-key-definition 'my-isearch-yank-word-or-char-from-beginning
+			     'isearch-yank-word-or-char
+			     isearch-mode-map))
+
+(add-hook 'isearch-mode-hook
+ (lambda ()
+   "Activate my customized Isearch word yank command."
+   (substitute-key-definition 'isearch-yank-word-or-char
+			      'my-isearch-yank-word-or-char-from-beginning
+			      isearch-mode-map)))
+
+;; Copy "things" at point
+(require 'thing-edit)
+(load-file "~/.InitScripts/Emacs/Configs/thing-edit_keybindings.el")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
---
+++ Emacs/Configs/thing-edit_keybindings.el
@@ -0,0 +1,17 @@
+(global-set-key (kbd "C-c k") 'thing-copy-word)
+(global-set-key (kbd "C-c å") 'thing-copy-line)
+(global-set-key (kbd "C-c ä") 'thing-copy-sentence)
+(global-set-key (kbd "C-c ö") 'thing-copy-symbol)
+
+;; thing-paste-word                paste word string around cursor.
+;; thing-copy-word                 copy word string around cursor.
+
+
+;; thing-paste-sentence            paste sentence string around cursor.
+;; thing-copy-sentence             copy sentence string around cursor.
+
+;; thing-paste-line                paste current line.
+;; thing-copy-line                 copy current line.
+
+;; thing-paste-comment             paste comment.
+;; thing-copy-comment              copy comment.
